595c77b6969a381a7e46336ac4b94a1f55f58884
Atik10671 <117341152+Atik10671@users.noreply.github.com>
2022-12-04 16:53:26 +0530

    Add files via upload

---
+++ additionarraymulti.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class additionarraymulti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a[][]= {{1,2,3,},{4,5,6,}};
+       int b[][]= {{1,3,7},{9,4,5}};
+       int c[][]=new int[2][3];
+       //adding and printing
+       for(int i=0;i<2;i++)
+       {
+    	   for(int j=0;j<3;j++)
+    	   {
+    		   c[i][j]=a[i][j]+b[i][j];
+    		   System.out.print(c[i][j]+" "); 
+    	   }
+    	   //just to print new line
+    	   System.out.println();
+       }
+	}
+
+}
